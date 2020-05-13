02c221236b728b2506fd40c17a12e0832d4aed89
Ronak Mehta <rmehta004@gmail.com>
2020-05-12 23:09:15 -0400

    Package reflects working UF instance. TO DO: compare against Jupyter version and Wills.

--- src/uncertainty_forest/uncertainty_forest.py
+++ src/uncertainty_forest/uncertainty_forest.py
@@ -36,19 +36,90 @@ class UncertaintyForest(BaseEstimator, ClassifierMixin):
         self.kappa = kappa
         self.base = base
 
-    def uf(X, y, n_estimators=300, max_samples=0.4, base=np.exp(1), kappa=3):
+    # def uf(X, y, n_estimators=300, max_samples=0.4, base=np.exp(1), kappa=3):
+
+    #     # Build forest with default parameters.
+    #     model = BaggingClassifier(
+    #         DecisionTreeClassifier(),
+    #         n_estimators=n_estimators,
+    #         max_samples=max_samples,
+    #         bootstrap=False,
+    #     )
+    #     model.fit(X, y)
+    #     n = X.shape[0]
+    #     K = model.n_classes_
+
+    #     cond_entropy = 0
+    #     for tree_idx, tree in enumerate(model):
+
+    #         # Find the indices of the training set used for partition.
+    #         sampled_indices = model.estimators_samples_[tree_idx]
+    #         unsampled_indices = np.delete(np.arange(0, n), sampled_indices)
+
+    #         # Randomly split the rest into voting and evaluation.
+    #         total_unsampled = len(unsampled_indices)
+    #         np.random.shuffle(unsampled_indices)
+    #         vote_indices = unsampled_indices[: total_unsampled // 2]
+    #         eval_indices = unsampled_indices[total_unsampled // 2 :]
+
+    #         # Store the posterior in a num_nodes-by-num_classes matrix.
+    #         # Posteriors in non-leaf cells will be zero everywhere
+    #         # and later changed to uniform.
+    #         node_counts = tree.tree_.n_node_samples
+    #         class_counts = np.zeros((len(node_counts), K))
+    #         for vote_index in vote_indices:
+    #             class_counts[
+    #                 tree.apply(X[vote_index].reshape(1, -1)).item(), y[vote_index]
+    #             ] += 1
+    #         row_sums = class_counts.sum(
+    #             axis=1
+    #         )  # Total number of estimation points in each leaf.
+    #         row_sums[row_sums == 0] = 1  # Avoid divide by zero.
+    #         class_probs = class_counts / row_sums[:, None]
+
+    #         # Make the nodes that have no estimation indices uniform.
+    #         # This includes non-leaf nodes, but that will not affect the estimate.
+    #         where_empty = np.argwhere(class_probs.sum(axis=1) == 0)
+    #         for elem in where_empty:
+    #             class_probs[elem] = [1 / K] * K
+
+    #         # Apply finite sample correction and renormalize.
+    #         where_0 = np.argwhere(class_probs == 0)
+    #         for elem in where_0:
+    #             class_probs[elem[0], elem[1]] = 1 / (
+    #                 kappa * class_counts.sum(axis=1)[elem[0]]
+    #             )
+    #         row_sums = class_probs.sum(axis=1)
+    #         class_probs = class_probs / row_sums[:, None]
+
+    #         # Place evaluation points in their corresponding leaf node.
+    #         # Store evaluation posterior in a num_eval-by-num_class matrix.
+    #         eval_class_probs = [class_probs[x] for x in tree.apply(X[eval_indices])]
+    #         eval_entropies = [entropy(posterior) for posterior in eval_class_probs]
+    #         cond_entropy += np.mean(eval_entropies)
+
+    def fit(self, X, y):
+
+        X, y = check_X_y(X, y)
+        check_classification_targets(y)
+        self.classes_, y = np.unique(y, return_inverse=True)
 
         # Build forest with default parameters.
         model = BaggingClassifier(
             DecisionTreeClassifier(),
-            n_estimators=n_estimators,
-            max_samples=max_samples,
+            n_estimators=self.n_estimators,
+            max_samples=self.max_samples,
             bootstrap=False,
         )
         model.fit(X, y)
         n = X.shape[0]
         K = model.n_classes_
 
+        _, counts = np.unique(y, return_counts=True)
+        est_H_Y = entropy(counts, base=self.base)
+
+        cond_entropy = 0
+        self.class_probs = []
         for tree_idx, tree in enumerate(model):
 
             # Find the indices of the training set used for partition.
@@ -60,75 +131,6 @@ class UncertaintyForest(BaseEstimator, ClassifierMixin):
             np.random.shuffle(unsampled_indices)
             vote_indices = unsampled_indices[: total_unsampled // 2]
             eval_indices = unsampled_indices[total_unsampled // 2 :]
-
-            # Store the posterior in a num_nodes-by-num_classes matrix.
-            # Posteriors in non-leaf cells will be zero everywhere
-            # and later changed to uniform.
-            node_counts = tree.tree_.n_node_samples
-            class_counts = np.zeros((len(node_counts), K))
-            for vote_index in vote_indices:
-                class_counts[
-                    tree.apply(X[vote_index].reshape(1, -1)).item(), y[vote_index]
-                ] += 1
-            row_sums = class_counts.sum(
-                axis=1
-            )  # Total number of estimation points in each leaf.
-            row_sums[row_sums == 0] = 1  # Avoid divide by zero.
-            class_probs = class_counts / row_sums[:, None]
-
-            # Make the nodes that have no estimation indices uniform.
-            # This includes non-leaf nodes, but that will not affect the estimate.
-            where_empty = np.argwhere(class_probs.sum(axis=1) == 0)
-            for elem in where_empty:
-                class_probs[elem] = [1 / K] * K
-
-            # Apply finite sample correction and renormalize.
-            where_0 = np.argwhere(class_probs == 0)
-            for elem in where_0:
-                class_probs[elem[0], elem[1]] = 1 / (
-                    kappa * class_counts.sum(axis=1)[elem[0]]
-                )
-            row_sums = class_probs.sum(axis=1)
-            class_probs = class_probs / row_sums[:, None]
-
-            # Place evaluation points in their corresponding leaf node.
-            # Store evaluation posterior in a num_eval-by-num_class matrix.
-            eval_class_probs = [class_probs[x] for x in tree.apply(X[eval_indices])]
-            eval_entropies = [entropy(posterior) for posterior in eval_class_probs]
-            cond_entropy = np.mean(eval_entropies)
-
-            return cond_entropy
-
-    def fit(self, X, y):
-
-        X, y = check_X_y(X, y)
-        check_classification_targets(y)
-        self.classes_, y = np.unique(y, return_inverse=True)
-
-        # Build forest with default parameters.
-        model = BaggingClassifier(
-            DecisionTreeClassifier(),
-            n_estimators=n_estimators,
-            max_samples=max_samples,
-            bootstrap=False,
-        )
-        model.fit(X, y)
-        n = X.shape[0]
-        K = model.n_classes_
-
-        self.class_probs = []
-
-        for tree_idx, tree in enumerate(model):
-
-            # Find the indices of the training set used for partition.
-            sampled_indices = model.estimators_samples_[tree_idx]
-            unsampled_indices = np.delete(np.arange(0, n), sampled_indices)
-
-            # Randomly split the rest into voting and evaluation.
-            total_unsampled = len(unsampled_indices)
-            np.random.shuffle(unsampled_indices)
-            vote_indices = unsampled_indices[: total_unsampled // 2]
-            # eval_indices = unsampled_indices[total_unsampled // 2 :]
 
             # Store the posterior in a num_nodes-by-num_classes matrix.
             # Posteriors in non-leaf cells will be zero everywhere
@@ -167,8 +169,10 @@ class UncertaintyForest(BaseEstimator, ClassifierMixin):
             # Store evaluation posterior in a num_eval-by-num_class matrix.
             eval_class_probs = [class_probs[x] for x in tree.apply(X[eval_indices])]
             eval_entropies = [entropy(posterior) for posterior in eval_class_probs]
-            cond_entropy = np.mean(eval_entropies)
+            cond_entropy += np.mean(eval_entropies)
 
+        self.cond_entropy = cond_entropy / self.n_estimators
+        self.mutual_info = est_H_Y - self.cond_entropy
         self.fitted = True
         return self
 
@@ -189,13 +193,13 @@ class UncertaintyForest(BaseEstimator, ClassifierMixin):
 
         X = check_array(X)
 
-        # Place evaluation points in their corresponding leaf node.
-        # Store evaluation posterior in a num_eval-by-num_class matrix.
-        eval_class_probs = [class_probs[x] for x in tree.apply(X[eval_indices])]
-        eval_entropies = [entropy(posterior) for posterior in eval_class_probs]
-        cond_entropy = np.mean(eval_entropies)
+        eval_class_probs = np.zeros((len(X), len(self.classes_)))
+        for tree_idx, tree in enumerate(self.model):
+            # Place evaluation points in their corresponding leaf node.
+            # Store evaluation posterior in a num_eval-by-num_class matrix.
+            eval_class_probs += [self.class_probs[tree_idx][x] for x in tree.apply(X)]
 
-        return cond_entropy
+        return eval_class_probs / n_estimators
 
     def estimate_cond_entropy(self):
 
